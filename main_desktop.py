c79aea192a33d8fef298b2013c9400de788c98e3
mudjaycker <tombutoyi@gmail.com>
2023-01-07 22:13:15 +0200

    basic design

--- main_desktop.py
+++ main_desktop.py
@@ -2,6 +2,10 @@ from desktop_ui.init_eel import *
 # from engine.speech2text import TamkaListener
 # from engine.text2speech import TamkaSpeaker
 
+@eel.expose
+def print_show():
+    print("show")
+
 eel.start('index.html')
 
 # TamkaListener().run_recognition(callable=TamkaSpeaker().say)

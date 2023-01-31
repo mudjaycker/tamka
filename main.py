f477edd6437deb7df6bda990e7ac5ea16548db3d
mudjaycker <tombutoyi@gmail.com>
2023-01-31 19:38:18 +0200

    webview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from desktop_ui.init_eel import *
-# import webview
+import webview
 import threading
 from engine.speech2text import TamkaListener
 from engine.text2speech import TamkaSpeaker
@@ -10,14 +10,14 @@ def start_speaker():
     TamkaListener().run_recognition(TamkaSpeaker().say)
 
 def start_eel():
-    eel.start("index.html", mode="chrome")
+    eel.start("index.html", mode=None)
     
-# eel_thread = threading.Thread(target=start_eel)
+eel_thread = threading.Thread(target=start_eel)
 speaker_thread = threading.Thread(target=start_speaker)
 
 speaker_thread.start()
-# eel_thread.start()
-eel.start("index.html", mode="chrome")
+eel_thread.start()
+# eel.start("index.html", mode="chrome")
 
-# webview.create_window('Tamka', 'http://localhost:8000/index.html')
-# webview.start()
+webview.create_window('Tamka', 'http://localhost:8000/index.html')
+webview.start()

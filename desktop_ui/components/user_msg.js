140cec49e0be87c1b52bb73553003583f73d0505
mudjaycker <tombutoyi@gmail.com>
2023-01-31 19:17:29 +0200

    sendMessage function to append user messages in frontend

---
+++ desktop_ui/components/user_msg.js
@@ -0,0 +1,33 @@
+function sendMessage(message) {
+  let element = new Element();
+  let conversation = element.select("conversation");
+
+  let userMsg = element.create({
+    classList: ["msg", "sent-msg"]
+  })
+
+  let paragraph = element.create({
+    type: "p",
+    text: message
+  })
+
+  element.append(userMsg, paragraph)
+  element.append(conversation, userMsg)
+}
+
+eel.expose(sendMessage)
+
+// L´ancienne méthode:
+/*
+function sendMessage(msg) {
+let conversation = document.getElementById('conversation')
+let userMsg = document.createElement("div")
+let classeNames = ["msg", "sent-msg"]
+classeNames.forEach(classeName => userMsg.classList.add(classeName))
+
+let paragraph = document.createElement("p")
+paragraph.textContent = msg
+userMsg.appendChild(paragraph)
+conversation.appendChild(userMsg)
+}
+*/

447463a37f3b7d5a17e8532d27039d8b13f995fc
mudjaycker <tombutoyi@gmail.com>
2023-02-20 15:59:43 +0200

    make app unable to listen itself

--- main.py
+++ main.py
@@ -4,23 +4,44 @@ import threading
 from engine.speech2text import TamkaListener
 from engine.text2speech import TamkaSpeaker
 from engine.word_bank_fr import datas
+from unsync import unsync
 
 
-@eel.expose
+def do_say(to_say):
+    receive_msg = threading.Thread(target=eel.receiveMsg, args=(to_say,))
+    speak = threading.Thread(target=TamkaSpeaker().say, args=(to_say,))
+    receive_msg.start()
+    speak.start()
+    receive_msg.join()
+    speak.join()
+                 
+    
+@unsync    
 def start_speaker(level):
-    TamkaListener().run_recognition(TamkaSpeaker().say, datas[level])
+    try:
+        text = datas[level].pop()
+        to_say = "dites: "+text
+        # do_say_thread = threading.Thread(target=do_say, args=(to_say,))
+        # recognize = threading.Thread(target=TamkaListener().run_recognition)
+        do_say(to_say)
+        TamkaListener().run_recognition()
+
+        
+    except IndexError:
+        to_say = "vous avez finis le niveau "+level
+        do_say_thread = threading.Thread(target=do_say, args=(to_say,))
+        do_say_thread.start()
 
 
+eel.expose(start_speaker)
 def start_eel():
     eel.start("index.html", mode=None)
 
 
 eel_thread = threading.Thread(target=start_eel)
-speaker_thread = threading.Thread(target=start_speaker)
 
-# speaker_thread.start()
 eel_thread.start()
-eel.start("index.html", mode="chrome")
+# eel.start("index.html", mode="chrome")
 
-# webview.create_window('Tamka', 'http://localhost:8000/index.html')
-# webview.start()
+webview.create_window('Tamka', 'http://localhost:8000/index.html')
+webview.start()

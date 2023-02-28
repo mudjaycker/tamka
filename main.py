7ed6bb942cf0904e7a1b4bc4c9fdf17a88aa59d1
mudjaycker <tombutoyi@gmail.com>
2023-02-28 18:22:01 +0200

    get_from_tamka table function

--- main.py
+++ main.py
@@ -6,20 +6,48 @@ from engine.text2speech import TamkaSpeaker
 from engine.word_bank_fr import datas
 from engine.views import TamkaView
 from unsync import unsync
+from pony.orm import db_session
+from datetime import date
+from typing import Any
+
+
+tamka_view = TamkaView()
+
+
+def get_from_tamka(type: str, when_is: Any, of_today: bool = True):
+    map_query = {
+        "level": lambda: tamka_view.get_where(lambda t: t.level == when_is
+                                              and t.date_of == date.today()),
+        "success": lambda: tamka_view.get_where(lambda t: t.success == when_is
+                                                and t.date_of == date.today()),
+    } if of_today else {
+        "level": lambda: tamka_view.get_where(lambda t: t.level == when_is),
+        "success": lambda: tamka_view.get_where(lambda t: t.success == when_is),
+    }
+
+    with db_session():
+        query = map_query.get(type)()
+        return query.count() if of_today else [{"text": q.text,
+                                                "level": q.level,
+                                                "success": q.success,
+                                                "date_of": q.date_of
+                                                } for q in query[:]]
+
+# print(get_from_tamka("success", when_is=True, of_today=False))
 
 
 def do_say(level, text):
-    receive_msg = threading.Thread(target=eel.systemSayToUser, args=(level, text))
+    receive_msg = threading.Thread(
+        target=eel.systemSayToUser, args=(level, text))
     speak = threading.Thread(target=TamkaSpeaker().say, args=(text,))
     receive_msg.start()
     speak.start()
     receive_msg.join()
     speak.join()
-                 
-    
-@unsync    
+
+
+@unsync
 def start_speaker(level):
-    tamka_view = TamkaView()
     try:
         text = datas[level].pop()
         to_say = "dites: "+text
@@ -30,19 +58,21 @@ def start_speaker(level):
             "success": False,
             "level": level
         }
-        
+
         if text.lower() == sayed:
             query_params["success"] = True
             tamka_view.set(**query_params)
         else:
             tamka_view.set(**query_params)
-                
+
     except IndexError:
         to_say = "vous avez finis le niveau "+"facile"
         do_say_thread = threading.Thread(target=do_say, args=(level, to_say))
         do_say_thread.start()
 
+
 eel.expose(start_speaker)
+
 
 def start_eel():
     eel.start("index.html", mode=None)

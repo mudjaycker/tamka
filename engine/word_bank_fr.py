a81909c4afaf690f64f529a2903640ba20904ba9
mudjaycker <tombutoyi@gmail.com>
2023-02-28 16:58:44 +0200

    update dict datas

--- engine/word_bank_fr.py
+++ engine/word_bank_fr.py
@@ -1,25 +1,18 @@
-simples = [
-    "la vie est belle",
-    "La paix vaut le risque",
-    "Peu importe la souffrance"
-]
-
-
 datas = {
-    "facile":[
-        "Unité, Travail, Progrès",
+    "easy":[
+        "Unité Travail Progrès",
         "La vie est belle",
         "Dieu est grand",
     ],
     
-    "moyen":[
+    "normal":[
         "Grandir est un choix",
         "Vivre est un droit",
         "La vie humaine est sacrée",
         "La terre tourne autour du soleil"
     ],
     
-    "difficile":[
+    "difficult":[
         "Le système solaire compte 9 planètes",
         "Les hommes mentent pas les chiffres",
         "Être négrophobe c'est rédouter de voir de la neige noire à noël",

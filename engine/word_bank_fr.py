b2be69370dba2c94c9fead2c382a11161965912a
mudjaycker <tombutoyi@gmail.com>
2023-03-04 20:19:29 +0200

    renaming keys of sentences datas  dict

--- engine/word_bank_fr.py
+++ engine/word_bank_fr.py
@@ -5,14 +5,14 @@ datas = {
         "Dieu est grand",
     ],
     
-    "normal":[
+    "medium":[
         "Grandir est un choix",
         "Vivre est un droit",
         "La vie humaine est sacrée",
         "La terre tourne autour du soleil"
     ],
     
-    "difficult":[
+    "hard":[
         "Le système solaire compte 9 planètes",
         "Les hommes mentent pas les chiffres",
         "Être négrophobe c'est rédouter de voir de la neige noire à noël",

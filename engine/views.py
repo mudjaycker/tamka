b7b3d0747397b878aae8e76dc089c296a44f6e88
mudjaycker <tombutoyi@gmail.com>
2023-03-04 20:03:09 +0200

    delete unused code

--- engine/views.py
+++ engine/views.py
@@ -15,21 +15,3 @@ class TamkaView:
         with orm.db_session():
             querry = Tamka.select(condition)
         return querry
-    
-    # def update(self, text:str, success:bool, date_of:datetime):
-        # with orm.db_session():
-            # tamka = Tamka.select(lambda t: t.text == text).first()
-            # tamka.set(success=success, date_of=date_of)
-
-
-#Full database
-# view = TamkaView()
-# with orm.db_session():
-    # x = view.get_where(lambda t: t.date_of == date.today() and t.success == False).count()
-    # print(x)
-
-# orm.set_sql_debug(True)
-# view.update(text="Unité Travail Progrès", success=True, date_of=datetime.now())
-# for texts in datas.items():
-    # for text in texts[1]:
-        # view.set(text=text, language="French", level=texts[0])

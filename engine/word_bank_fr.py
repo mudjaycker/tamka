657c4bdee3c98f3b57f3e77be7029a58293df5ce
mudjaycker <tombutoyi@gmail.com>
2023-03-09 16:39:26 +0200

    add new data for languages

--- engine/word_bank_fr.py
+++ engine/word_bank_fr.py
@@ -1,18 +1,18 @@
 french_datas = {
-    "easy":[
+    "easy": [
         "Unité Travail Progrès",
         "La vie est belle",
         "Dieu est grand",
     ],
-    
-    "medium":[
+
+    "medium": [
         "Grandir est un choix",
         "Vivre est un droit",
         "La vie humaine est sacrée",
         "La terre tourne autour du soleil"
     ],
-    
-    "hard":[
+
+    "hard": [
         "Le système solaire compte 9 planètes",
         "Les hommes mentent pas les chiffres",
         "Être négrophobe c'est rédouter de voir de la neige noire à noël",
@@ -21,6 +21,21 @@ french_datas = {
 
 english_datas = {
     "easy": [
-        "Charity begin at home"
+        "Charity begins at home",
+        "The train was late",
+        "He had a great army"
+    ],
+
+    "medium": [
+        "I waited for the train",
+        "I am buying a new pair of shoes",
+        "The earth is spherical"
+    ],
+
+    "hard": [
+        "I looked for mother and father at the bus station",
+        "There were many others who wanted to become king",
+        "My brother sometimes forgets his keys"
+
     ]
 }

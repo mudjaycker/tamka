cc71a1692d3abfc09393d2562f6b88a393092ec0
mudjaycker <tombutoyi@gmail.com>
2023-03-19 16:52:07 +0200

    service worker

--- desktop_ui/serviceWorker.js
+++ desktop_ui/serviceWorker.js
@@ -1,7 +1,21 @@
 cacheName = "Tamka-v1";
-const files = ["./index.html"];
+const files = [
+  "./index.html",
+  "./assets/images/Hexagon.svg",
+  "./assets/style/style.css",
+  "./third_parts/chart.js",
+  "./third_parts/feather_icons.min.js",
+  "./utils/storage.js",
+  "./utils/element.js",
+  "./utils/say-to-system.js",
+  "./utils/say-to-user.js",
+];
 function setInCache() {
   caches.open(cacheName).then((result) => {
-    result.addAll(files)
+    result.addAll(files);
   });
 }
+
+self.addEventListener("install", (e) => {
+  caches.match(e.request).then((result) => result);
+});
--- desktop_ui/utils/say-to-user.js
+++ desktop_ui/utils/say-to-user.js
@@ -1,20 +1,19 @@
-function systemSayToUser(toSay) {
-
-  let element = new Element()
-  let conversation = element.select("conversation")
+function systemSayToUser(level, toSay) {
+  let element = new Element();
+  let conversation = element.select("conversation");
   let receivedMsg = element.create({
     classList: ["msg", "received-msg"],
-  })
+  });
   let paragraph = element.create({
     type: "paragraph",
     text: toSay,
-  })
+  });
 
-  element.append(receivedMsg, paragraph)
-  element.append(conversation, receivedMsg)
+  element.append(receivedMsg, paragraph);
+  element.append(conversation, receivedMsg);
 
-  let centerSide = document.getElementById('center')
-  centerSide.scrollTop = centerSide.scrollHeight
+  let centerSide = document.getElementById("center");
+  centerSide.scrollTop = centerSide.scrollHeight;
 }
 
-eel.expose(systemSayToUser)
+eel.expose(systemSayToUser);

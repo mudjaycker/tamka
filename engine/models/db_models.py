d3d5727711e1e953d3d89f9ff1485a6e887d59d0
mudjaycker <tombutoyi@gmail.com>
2023-04-14 22:46:57 +0200

    using types

--- engine/models/db_models.py
+++ engine/models/db_models.py
@@ -1,20 +1,21 @@
-from pony import orm
+# mypy: disable-error-code="name-defined"
+from pony import orm 
 from datetime import date
+
 
 db = orm.Database()
 
 
 class Tamka(db.Entity):
-    text = orm.Required(str)
-    success = orm.Optional(bool)
-    date_of = orm.Required(date, default=date.today())
-    language = orm.Required(str)
-    level = orm.Required(str)
-    
+    text: str = orm.Required(str)
+    success: bool | None = orm.Optional(bool)
+    date_of: date = orm.Required(date, default=date.today())
+    language: str = orm.Required(str)
+    level: str = orm.Required(str)
 
 class User(db.Entity):
-    username = orm.Required(str)
-    password = orm.Required(bytes)
+    username: str = orm.Required(str)
+    password: bytes = orm.Required(bytes)
 
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
--- engine/models/views.py
+++ engine/models/views.py
@@ -1,37 +1,44 @@
+from pony.orm.core import Query # for typing
+
 from pony import orm
-from . db_models import Tamka, User
+from  . db_models import Tamka, User, date
 import bcrypt
+from typing import Callable, Sequence, Union
+
+Users = Union[Query , Sequence[User] ]
+Tamkas = Union[Query,  Sequence[Tamka]]
+
 
 class TamkaView:
-    def set(self, **kwargs):
+    def set(self, text: str, level: str, success: bool, language:str, date_of: date=date.today()):
         """text: str, success: Optional[bool], language: str, 
             level: str, date_of: Optional[datetime]"""
-        
+            
         with orm.db_session():
-            Tamka(**kwargs)
+            Tamka(text=text, language=language, level=level, date_of=date_of)
 
-    def get_where(self, condition) -> Tamka:
+    def get_where(self, condition: Callable[[Tamka], bool]):
         with orm.db_session():
-            querry = Tamka.select(condition)
-        return querry
+            query: Tamkas = Tamka.select(condition)
+        return query
 
 
 class UserView:
-    def set(self, username, password):
-        password = password.encode('utf-8')
-        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-        
+    def set(self, username: str, password: str):
+        password_encoded = password.encode('utf-8')
+        hashed = bcrypt.hashpw(password_encoded, bcrypt.gensalt())
+
         with orm.db_session():
             User(username=username, password=hashed)
-    
-    def is_user(self, username, password):
-        password = password.encode("utf-8")
+
+    def is_user(self, username: str, password: str) -> bool:
+        password_encoded = password.encode("utf-8")
         with orm.db_session():
-            querry = User.select(lambda x: x.username == username)
-            user = querry[:][0]
-            
-            return bcrypt.checkpw(password, user.password)
-            
+            query: Users = User.select(lambda x: x.username == username)
+            print(query)
+            user = query[:][0]
+
+            return bcrypt.checkpw(password_encoded, user.password)
 
 
 # u = UserView()

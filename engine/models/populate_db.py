12da6e152e32c96b44b52a9e052aa52b9f21dae4
mudjaycker <tombutoyi@gmail.com>
2023-05-29 17:41:34 +0200

    populate db

---
+++ engine/models/populate_db.py
@@ -0,0 +1,8 @@
+# from word_bank_fr import datas
+# from views import TamkaView
+
+# tamka = TamkaView()
+# for language, levels in datas.items():
+    # for level, texts in levels.items():
+        # for text in texts:
+            # tamka.set(text=text, level=level, language=language)

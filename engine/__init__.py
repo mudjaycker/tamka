3387c0bd24aee415c95c7d0f6715b6c65bcd3c50
mudjaycker <tombutoyi@gmail.com>
2023-05-31 12:58:37 +0200

    remove unused imports

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,16 +1,6 @@
 # mypy: disable-error-code="no-any-return"
 import eel
 from pathlib import Path
-
-from copy import deepcopy
-import threading
-from pony.orm import db_session
-from random import shuffle
-from datetime import date
-
-from .stt_tts.speech2text import TamkaListener
-from .stt_tts.text2speech import TamkaSpeaker
-from . models.word_bank_fr import datas, datas_copy
 from .models.views import TamkaView, UserView, GameView
 from typing import Final
 import eel

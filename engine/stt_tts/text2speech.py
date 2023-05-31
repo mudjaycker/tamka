96a72905f7c81dc44f9ddcfb438a9474c63ebba2
mudjaycker <tombutoyi@gmail.com>
2023-05-31 15:02:06 +0200

    update TamkaSpeaker

--- engine/stt_tts/text2speech.py
+++ engine/stt_tts/text2speech.py
@@ -4,33 +4,37 @@ from . ubuntu_speaker import speaker
 
 
 class TamkaSpeaker:
-    def __init__(self, language: str = "français") -> None:
+    def __init__(self, message: str, language: str = "français") -> None:
         self.rate = 130,
+        self.message = message
         self.language = language
-        self.win_lang = language
         self.engine = pyttsx3.init()
         self.engine.setProperty('self.rate', self.rate)
 
-        if platform.system() == "Windows":
-            self.set_language()
-
-    def set_language(self) -> bool | None:
+    def __set_language_windows(self, language: str) -> bool | None:
         for voice in self.engine.getProperty('voices'):
-            if self.language in voice.id:
+            if language in voice.id:
                 self.engine.setProperty('voice', voice.id)
                 return True
 
         raise RuntimeError(f"Language '{self.language}' not supported")
 
-    def say(self, message: str) -> None:
-        if platform.system() == "Linux":
-            speaker(message, self.language)
-        else:
-            if self.language == "français":
-                self.win_lang = "fr-fr".upper()
-            else:
-                self.win_lang = "eng-us".upper()
+    def __winows_say(self):
+        lang_map = {
+            "français": "FR-FR",
+            "english": "EN-US",
+        }
+        self.__set_language_windows(lang_map[self.language])
+        self.engine.say(self.message)
+        self.engine.runAndWait()
 
-            self.engine.say(message)
-            self.engine.runAndWait()
-        # self.engine.stop()
+    def __linux_say(self) -> None:
+        speaker(self.message, self.language)
+
+    def speak(self):
+        speaker_map = {
+            "Linux":  self.__linux_say,
+            "Windows": self.__winows_say
+        }
+        operating_system = platform.system()
+        speaker_map[operating_system]()

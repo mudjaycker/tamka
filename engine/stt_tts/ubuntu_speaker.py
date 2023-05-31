551c6b6e596fd504bad7f82e327887ddc2031997
mudjaycker <tombutoyi@gmail.com>
2023-05-31 15:02:20 +0200

    update ubuntu speaker

--- engine/stt_tts/ubuntu_speaker.py
+++ engine/stt_tts/ubuntu_speaker.py
@@ -4,11 +4,15 @@ import os
 
 
 def speaker(text: str, language: str) -> None:
-    """French speaker"""
-    if language == "français":
-        cmd_speaker = f"""pico2wave -l fr-FR -w tempo.wav "{text}" &&
-        play -qV0 tempo.wav treble 20 gain -l 6 tempo 0.7 """
-    else:
-        cmd_speaker = f"""pico2wave -l en-US -w tempo.wav "{text}"&&
-        play -qV0 tempo.wav treble 5 gain -l 5 tempo 0.85 """
-        os.system(cmd_speaker)
+    """Linux speaker"""
+    target_file = "tempo.wav"
+    speaker_lang_map = {
+        "français": f"""pico2wave -l fr-FR -w {target_file} "{text}" &&                              ─╯
+                        play -qV0 {target_file} treble 20 gain -l 6 tempo 0.7
+                     """,
+
+        "english": f"""pico2wave -l en-US -w {target_file} "{text}" &&
+                       play -qV0 {target_file} treble 5 gain -l 5 tempo 0.85
+                    """,
+    }
+    os.system(speaker_lang_map[language])
